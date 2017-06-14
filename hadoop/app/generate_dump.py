7e699a952402dd7f22a3d5c736668199926e6b40
maiktheknife <maiktheknife@gmail.com>
2017-06-14 11:13:30 +0200

    inline generate_dump.py

--- hadoop/app/generate_dump.py
+++ hadoop/app/generate_dump.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 with open('data.txt', 'w') as output_file:
-    data = range(0, 100_000_000)
-    data = map(lambda x: str(x), data)
-    output_file.write(",".join(data))
+    #data = range(0, 100_000_000)
+    #data = map(lambda x: str(x), data)
+    #output_file.write(",".join(data))
+    output_file.write(",".join(map(lambda x: str(x), range(0, 100_000_000))))
