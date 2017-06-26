059cb5566f45003699f1eeba5cd4a48e77858747
maiktheknife <maiktheknife@gmail.com>
2017-06-26 12:08:51 +0200

    ...

--- spark/app/wordcount/wordcount.py
+++ spark/app/wordcount/wordcount.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import sys
-import re
 
 from pyspark import SparkContext, SparkConf
 
@@ -11,9 +10,6 @@ if __name__ == "__main__":
 	conf = SparkConf().setAppName("Spark Count")
 	sc = SparkContext(conf=conf)
 
-	input_file = sys.argv[1]
-	output_file = sys.argv[2]
-
 	# read in text file and split each document into words
 	words = sc.textFile(sys.argv[1]) \
 		.flatMap(lambda line: line.split(" ")) \
@@ -21,18 +17,35 @@ if __name__ == "__main__":
 		.reduceByKey(lambda v1, v2: v1 + v2) \
 		.collect()
 
-	# words = sc.textFile(input_file) \
-	# 	.flatMap(lambda line: re.split("[ ,;.:\-!?\t\n]+", line)) \
-	# 	.map(lambda word: word.strip()) \
-	# 	.filter(lambda word: True if word else False) \
-	# 	.map(lambda word: (word, 1))
-	# print words
-	#
-	# x = words.reduceByKey(lambda v1, v2: v1 + v2)
-	# print x
-	#
-	# y = x.collect()
-	# print y
-	#
 	print repr(words)[1:-1]
-	# words.saveAsTextFile(output_file)
+
+#
+# import sys
+# import re
+#
+# from pyspark import SparkContext, SparkConf
+#
+# if __name__ == "__main__":
+#
+# 	# create Spark context with Spark configuration
+# 	conf = SparkConf().setAppName("Spark Count")
+# 	sc = SparkContext(conf=conf)
+#
+# 	input_file = sys.argv[1]
+# 	output_file = sys.argv[2]
+#
+# 	words = sc.textFile(input_file) \
+# 		.flatMap(lambda line: re.split("[ ,;.:\-!?\t\n]+", line)) \
+# 		.map(lambda word: word.strip()) \
+# 		.filter(lambda word: True if word else False) \
+# 		.map(lambda word: (word, 1))
+# 	print words
+#
+# 	x = words.reduceByKey(lambda v1, v2: v1 + v2)
+# 	print x
+#
+# 	y = x.collect()
+# 	print y
+#
+# 	print repr(words)[1:-1]
+# 	# words.saveAsTextFile(output_file)
