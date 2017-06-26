e2fa734c29104e2443480ba6ee3c7bf5bc318400
maiktheknife <maiktheknife@gmail.com>
2017-06-26 11:30:53 +0200

    save progress

--- spark/app/wordcount/wordcount.py
+++ spark/app/wordcount/wordcount.py
@@ -15,17 +15,24 @@ if __name__ == "__main__":
 	output_file = sys.argv[2]
 
 	# read in text file and split each document into words
-	# words = sc.textFile(sys.argv[1]) \
-	# 	.flatMap(lambda line: line.split(" ")) \
-	# 	.map(lambda word: (word, 1)) \
-	# 	.reduceByKey(lambda v1, v2: v1 + v2)
-
-	words = sc.textFile(input_file) \
-		.flatMap(lambda line: re.split("[ ,;.:\-!?\t\n]+", line)) \
-		.map(lambda word: word.strip()) \
-		.filter(lambda word: True if word else False) \
+	words = sc.textFile(sys.argv[1]) \
+		.flatMap(lambda line: line.split(" ")) \
 		.map(lambda word: (word, 1)) \
-		.reduceByKey(lambda v1, v2: v1 + v2)
+		.reduceByKey(lambda v1, v2: v1 + v2) \
+		.collect()
 
+	# words = sc.textFile(input_file) \
+	# 	.flatMap(lambda line: re.split("[ ,;.:\-!?\t\n]+", line)) \
+	# 	.map(lambda word: word.strip()) \
+	# 	.filter(lambda word: True if word else False) \
+	# 	.map(lambda word: (word, 1))
+	# print words
+	#
+	# x = words.reduceByKey(lambda v1, v2: v1 + v2)
+	# print x
+	#
+	# y = x.collect()
+	# print y
+	#
 	print repr(words)[1:-1]
 	# words.saveAsTextFile(output_file)
