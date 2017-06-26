ec556a8094c47371bc4ff40c41fe21e18c9edef0
maiktheknife <maiktheknife@gmail.com>
2017-06-26 12:34:06 +0200

    use new example

--- spark/app/wordcount/wordcount.py
+++ spark/app/wordcount/wordcount.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import sys
+import re
 
 from pyspark import SparkContext, SparkConf
 
@@ -13,42 +14,10 @@ if __name__ == "__main__":
 	input_file = sys.argv[1]
 	output_file = sys.argv[2]
 
-	# read in text file and split each document into words
-	words = sc.textFile(input_file) \
-		.flatMap(lambda line: line.split(" ")) \
+	sc.textFile(input_file) \
+		.flatMap(lambda line: re.split("[ ,;.:\-!?\t\n]+", line)) \
+		.map(lambda word: word.strip()) \
+		.filter(lambda word: True if word else False) \
 		.map(lambda word: (word, 1)) \
-		.reduceByKey(lambda v1, v2: v1 + v2)
-
-	words.saveAsTextFile(output_file)
-	# print repr(words)[1:-1]
-
-#
-# import sys
-# import re
-#
-# from pyspark import SparkContext, SparkConf
-#
-# if __name__ == "__main__":
-#
-# 	# create Spark context with Spark configuration
-# 	conf = SparkConf().setAppName("Spark Count")
-# 	sc = SparkContext(conf=conf)
-#
-# 	input_file = sys.argv[1]
-# 	output_file = sys.argv[2]
-#
-# 	words = sc.textFile(input_file) \
-# 		.flatMap(lambda line: re.split("[ ,;.:\-!?\t\n]+", line)) \
-# 		.map(lambda word: word.strip()) \
-# 		.filter(lambda word: True if word else False) \
-# 		.map(lambda word: (word, 1))
-# 	print words
-#
-# 	x = words.reduceByKey(lambda v1, v2: v1 + v2)
-# 	print x
-#
-# 	y = x.collect()
-# 	print y
-#
-# 	print repr(words)[1:-1]
-# 	# words.saveAsTextFile(output_file)
+		.reduceByKey(lambda v1, v2: v1 + v2) \
+		.saveAsTextFile(output_file)
