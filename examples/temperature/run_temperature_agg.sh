636ed5db2f79a6b605d7b9cce6683be93d5bfaf4
maiktheknife <maiktheknife@gmail.com>
2017-07-12 13:05:54 +0200

    fix return code usage

--- examples/temperature/run_temperature_agg.sh
+++ examples/temperature/run_temperature_agg.sh
@@ -20,8 +20,8 @@ mvn clean package
 yarn jar target/hadoop-prototyp-1.0-SNAPSHOT.jar de.menzel.hadoop.temperature.TemperatureAggregator /years /years_out
 
 # get results from HDFS
-(hdfs dfs -test -e /years_out/_SUCCESS)& echo $?
+hdfs dfs -test -e /years_out/_SUCCESS
+success=$(echo $?)
+echo "Success '$success'"
 
 hdfs dfs -tail /years_out/part-r-00000
-
-#xxx=$(hdfs dfs -test -e /years_out/_SUCCESS)?
--- examples/wordcount/run_wordcount.sh
+++ examples/wordcount/run_wordcount.sh
@@ -9,5 +9,9 @@ hdfs dfs -ls /inputdir
 # run application
 spark-submit --master yarn --deploy-mode cluster wordcount.py /inputdir/inputfile_new /outputdir
 
+hdfs dfs -test -e /outputdir/_SUCCESS
+success=$(echo $?)
+echo "Success '$success'"
+
 # get results from HDFS
 hdfs dfs -tail /outputdir/part-00000
